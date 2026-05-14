85ac1cc48f379e13ce5e7ce8d07618eb36af2efe
gfraguas98 <gfraguas@ayesa.com>
2026-05-14 13:47:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,40 +5,50 @@ const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
+
+// Static files
 app.use(express.static('public'));
 
-// HOME
+// Root
 app.get('/', (req, res) => {
     res.send('SFMC Custom Activity Running');
 });
 
-// SERVIR CONFIG.JSON
+// CONFIG.JSON
 app.get('/config.json', (req, res) => {
+    res.type('application/json');
     res.sendFile(path.join(__dirname, 'config.json'));
 });
 
-// EXECUTE JOURNEY
-app.post('/execute', (req, res) => {
-
-    console.log('Execute called');
-    console.log(JSON.stringify(req.body, null, 2));
-
-    return res.status(200).json({
-        success: true
-    });
-});
-
-// JOURNEY CONFIG METHODS
+// JOURNEY ENDPOINTS
 app.post('/save', (req, res) => {
-    return res.status(200).json({});
+    console.log('SAVE');
+    res.status(200).json({});
 });
 
 app.post('/publish', (req, res) => {
-    return res.status(200).json({});
+    console.log('PUBLISH');
+    res.status(200).json({});
 });
 
 app.post('/validate', (req, res) => {
-    return res.status(200).json({});
+    console.log('VALIDATE');
+    res.status(200).json({});
+});
+
+app.post('/stop', (req, res) => {
+    console.log('STOP');
+    res.status(200).json({});
+});
+
+app.post('/execute', (req, res) => {
+
+    console.log('EXECUTE');
+    console.log(JSON.stringify(req.body, null, 2));
+
+    res.status(200).json({
+        success: true
+    });
 });
 
 const PORT = process.env.PORT || 3000;
